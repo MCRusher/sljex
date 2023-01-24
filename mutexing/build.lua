13a02f26d650e9676562db44bdd33068471f42db
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2023-01-24 15:16:06 -0500

    Modify code to only use pthreads

--- mutexing/build.lua
+++
@@ -1,2 +0,0 @@
-#!/bin/lua
-os.execute("gcc -O2 -DEXPORT -pthread -shared -fPIC mutexing.c -o ../libs/libmutexing.so")
--- tldata/build.lua
+++
@@ -1,2 +0,0 @@
-#!/bin/lua
-os.execute("gcc -O2 -DEXPORT -pthread -shared -fPIC tldata.c -o ../libs/libtldata.so")
